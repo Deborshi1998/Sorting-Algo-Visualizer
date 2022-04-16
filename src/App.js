d8f8922af7f443fb8a554c4f5cf78445e7a4e52d
Debarshi Raj Basumatary <basumatary18@gmail.com>
2022-04-17 01:26:46 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import React from "react";
 import SortingVisualizer from "./components/SortingVisualizer";
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        SortingVisualizer / >
-        <
-        /div>
+    return ( 
+    <div className = "App" >
+        <SortingVisualizer / >
+    </div>
     );
 }
 
