1bb59021f0e6adbf1b862e8cce7f5b1e137a6a2b
Debarshi Raj Basumatary <basumatary18@gmail.com>
2022-04-17 01:38:09 +0530

    after chit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import "./App.css";
-import React from "react";
-import SortingVisualizer from "./components/SortingVisualizer";
+
+import './App.css';
 
 function App() {
+
     return ( 
     <div className = "App" >
         <SortingVisualizer / >
     </div>
     );
+
 }
 
 export default App;
